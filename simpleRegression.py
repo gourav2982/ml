0ab512936b630871e8a6e434f4d5172178be2b9e
Gourav Kumar <91111185+gourav2982@users.noreply.github.com>
2023-07-19 21:20:48 +0530

    simple regression

---
+++ simpleRegression.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+dataset = pd.read_csv('Dataset/Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# we use linear regression model on our training set using sklearn lib
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# we predict the dependent variable of training set
+y_pred = regressor.predict(X_test)
+
+# we plot the graph
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+print(regressor.predict([[4]]))
